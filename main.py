64e81138c3fe0ee4cf99b4fc012c3a98e0c4292d
Calvin Chu <calvinhoyinchu@gmail.com>
2021-01-23 06:57:06 +0800

    reformatted comments

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ class Acorn:
         login_session = self.bot
         login_session.get('https://acorn.utoronto.ca/')
         time.sleep(4)
-        # Enter utorid and password to ACORN
-        username = login_session.find_element_by_id('username')
+        username = login_session.find_element_by_id(
+            'username')  # Enter utorid and password to ACORN
         password = login_session.find_element_by_id('password')
         username.clear()
         password.clear()
@@ -57,78 +57,57 @@ class Acorn:
         password.send_keys(self._ut_password)
         password.send_keys('\ue006')
         time.sleep(8)
-        # Go to course enrollment page in ACORN after logging in
-        login_session.get('https://acorn.utoronto.ca/sws/#/courses/0')
+        login_session.get(
+            'https://acorn.utoronto.ca/sws/#/courses/0')  # Go to course enrollment page in ACORN after logging in
 
     def find_course(self, co: str, term: str, lecture: List[str]) -> \
             Optional[bool]:
         """
         Automated a bot and search if there is available space for lecture
         sections in lecture. Returns True if at least one of the lecture section
-        has an available space
+        has an available space.
         """
         time.sleep(3)
         login_session = self.bot
-        # Formulate official course code using co and term
-        course_code = co.upper() + ' ' + term.upper()
-        # Find where to input course code in course enrollment page
-        course = login_session.find_element_by_id('typeaheadInput')
+        course_code = co.upper() + ' ' + term.upper()  # Formulate official course code using co and term
+        course = login_session.find_element_by_id(
+            'typeaheadInput')  # Find where to input course code in course enrollment page
         course.clear()
         course.send_keys(co.upper())
         time.sleep(2)
         course.click()
-        # Bot to find and click on the correct on the right course under the
-        # javascript course search bar
         login_session.find_element_by_xpath(
+            # Find and click on the course under the javascript course search bar
             "//*/span[contains(text(),'" + course_code + "')]").click()
         time.sleep(5)
-        # Initialized variable "available" to keep track if space is available
-        # in any of the chosen lecture sections
-        available = False
-        # Loop through each lecture section in lecture and determine if free
-        # space is available
-        for lec in lecture:
-            lecture_code = lec[0:3].upper() + '-' + lec[3:].upper()
-            # number_of_space is the variable that shows how many space is
-            # available at that lecture section currently
+        available = False  # Initialized variable "available" to keep track if space is available
+        for lec in lecture:  # Loop through each lecture section in lecture and determine if an open slot is available
+            lecture_code = lec[0:3].upper() + '-' + lec[
+                                                    3:].upper()  ## lecture_code represents the lecture section
             number_of_space = login_session.find_element_by_id(
+                # number_of_space is the variable that shows how many space is available at that lecture section currently
                 lecture_code).find_element_by_css_selector(
                 "span[data-ng-bind-html='getSpaceAvailabilityMessage"
                 "(currentCourse, info, modalCourse.isPlannedItem)']")
             try:
-                # c is a string that displays information regarding space
-                c = number_of_space.text
-                # n is the first character of the text, if and only if it is an
-                # integer that is not 0, it means at least a space is available
-                # and will run self.send_email to send an email alert
-                n = int(c[0])
-                if n != 0:
+                c = number_of_space.text  # c is a string that displays information regarding space
+                n = int(c[0])  # n is the first character of the text
+                if n != 0:  # if and only if n is an integer that is not 0, it means at least a space is available and will run self.send_email to send an email alert
                     print('ava')
                     self.send_email(co, term, lec, c)
-                    # available to set to True and will return True as a space
-                    # is available
                     available = True
-            # If n is unable to changed from str to int then it is another
-            # indication that the lecture section is currently full.
-            except ValueError:
+            except ValueError:  # If n is not an integer then lecture section is currently full
                 print("FULL")
-            # Occasionally a known bug will occur where number_of_space.text
-            # is an empty str and therefore IndexError will happen when trying
-            # to access index 0 of c.
-            except IndexError:
+            except IndexError:  # Occasionally a known bug will occur where number_of_space.text is empty string
                 print(
                     "Unable to check availability due to glitch on webscrapping "
                     "on Acorn, will try again.")
         time.sleep(2)
-        # If available is True, whole Firefox browser will close and return True
-        if available:
+        if available:  # Open slot is found and email has been sent. Firefox browser will close and return True.
             login_session.close()
             return True
-        # If no space is found then bot will close the popup specified course
-        # enrollment page and stay IDLE at the general course enrollment ACORN
-        # page
-        login_session.find_element_by_class_name("close.icon-cancel").click()
-        time.sleep(4)
+        login_session.find_element_by_class_name(
+            "close.icon-cancel").click()  # If no space is found then bot will close the popup specified course enrollment page and stay IDLE
 
     def send_email(self, co: str, term: str, lecture: str,
                    number_of_space: str) -> None:
